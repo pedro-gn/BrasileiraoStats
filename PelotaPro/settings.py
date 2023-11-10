5be0e51638732a6305267c25b9cbcd73c63f10e2
Pedro Garcia <pedrognunes139@gmail.com>
2023-11-09 23:10:31 -0300

    Deploy version 2

--- PelotaPro/settings.py
+++ PelotaPro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-occw*u8atl6drm+9p2x+=-7-p9j5^qr8l-9a^en0(pm7mqqjvy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'brasileirao-stats.onrender.com']
 
 
 # Application definition
